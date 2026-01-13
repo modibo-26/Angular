31e533523128b4b4d4ffc890428dd88d8f4769cb
Modibo <tr.modibo26@gmail.com>
2026-01-13 09:13:58 +0100

    partie 3

--- snapface/src/app/app.ts
+++ snapface/src/app/app.ts
@@ -12,16 +12,32 @@ import { FaceSnapComponent } from './face-snap/face-snap';
 })
 export class App implements OnInit {
   protected readonly title = signal('snapface');
-  mySnap!: FaceSnap;
+  faceSnaps!: FaceSnap[];
 
-
-  ngOnInit() {
-    this.mySnap = new FaceSnap(
-      'Archibald',
-      'Mon meilleur ami depuis tout petit !',
-      'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
-      new Date(),
-      10
-    );
+  ngOnInit(): void {
+    this.faceSnaps = [
+      new FaceSnap(
+        'Archibald',
+        'Mon meilleur ami depuis toujours !',
+        'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+        new Date(),
+        10
+      ),
+      new FaceSnap(
+        'Three Rock Mountain',
+        'Un endroit magnifique pour les randonnées.',
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Three_Rock_Mountain_Southern_Tor.jpg/2880px-Three_Rock_Mountain_Southern_Tor.jpg',
+        new Date(),
+        6
+      ),
+      new FaceSnap(
+        'Un bon repas',
+        'Mmmh que c\'est bon !',
+        'https://wtop.com/wp-content/uploads/2020/06/HEALTHYFRESH.jpg',
+        new Date(),
+        156
+      )
+    ];
+    this.faceSnaps[1].setLocation('à la montagne');
   }
 }
--- snapface/src/app/models/face-snap.ts
+++ snapface/src/app/models/face-snap.ts
@@ -1,15 +1,22 @@
 export class FaceSnap {
-  constructor(public title: string,
-              public description: string,
-              public imageUrl: string,
-              public createdAt: Date,
-              public snaps: number) {}
 
-  addSnap(): void {
-    this.snaps++;
-  }
+    location?: string;
 
-  removeSnap(): void {
-    this.snaps--;
-  }
+    constructor(public title: string,
+                public description: string,
+                public imageUrl: string,
+                public createdAt: Date,
+                public snaps: number) {}
+
+    addSnap(): void {
+        this.snaps++;
+    }
+
+    removeSnap(): void {
+        this.snaps--;
+    }
+
+    setLocation(location: string) {
+        this.location = location;
+    }
 }
